307f265a41a2f86db31a823f5ec128a7ebfc9898
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2018-09-25 16:39:51 -0400

    added question in index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ mapMarker.style.width = "32px";
 mapMarker.style.height = "39px";
 mapMarker.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
 
+//before copying and pasting exact coordinates array into setLngLat method, I passed map.center
+//this threw an error saying that setLngLat expected an object, not an array
+//then I created the obj below, and it failed saying that map.center[0] was undefined
+//what happened here?
+
 // const lngLatObj = {
 //     lng: map.center[0],
 //     lat: map.center[1]
